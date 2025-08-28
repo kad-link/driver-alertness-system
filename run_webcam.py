bada2e8b051df8aabb86e96ecd1caff16a0953f6
Sri CHaRan <sricharanchittineni@gmail.com>
2025-08-28 18:22:44 +0530

    webrtc

--- run_webcam.py
+++ run_webcam.py
@@ -1,36 +1,53 @@
 import cv2
 import numpy as np
-from tensorflow.keras.models import load_model
 from collections import deque
+from tensorflow.keras.models import load_model
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
 
+# Load trained model
 model = load_model("drowsiness_model_colab.h5")
+
+# Parameters
 IMG_SIZE = (64, 64)
 SEQUENCE_LENGTH = 20
-frames = deque(maxlen=SEQUENCE_LENGTH)
-cap = cv2.VideoCapture(0)
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
+st.set_page_config(page_title="Driver Alertness System", layout="centered")
+st.title("Driver Alertness Detection (Real-time)")
 
-    img = cv2.resize(frame, IMG_SIZE)
-    img = img / 255.0
-    frames.append(img)
+class VideoTransformer(VideoTransformerBase):
+    def __init__(self):
+        self.frames = deque(maxlen=SEQUENCE_LENGTH)
 
-    if len(frames) == SEQUENCE_LENGTH:
-        X = np.expand_dims(frames, axis=0)   
-        pred = model.predict(X)[0][0]
-        label = "DROWSY" if pred > 0.5 else "AWAKE"
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
 
-        color = (0,0,255) if label == "DROWSY" else (0,255,0)
-        cv2.putText(frame, label, (50,50),
+        resized = cv2.resize(img, IMG_SIZE)
+        resized = resized / 255.0
+        self.frames.append(resized)
+
+        label = "WAITING..."
+        color = (255, 255, 0)
+
+        if len(self.frames) == SEQUENCE_LENGTH:
+            X = np.expand_dims(self.frames, axis=0)
+            pred = model.predict(X)[0][0]
+
+            if pred > 0.5:
+                label = "DROWSY"
+                color = (0, 0, 255)
+            else:
+                label = "AWAKE"
+                color = (0, 255, 0)
+
+       
+        cv2.putText(img, label, (30, 50),
                     cv2.FONT_HERSHEY_SIMPLEX, 1.2, color, 3)
 
-    cv2.imshow("Drowsiness Detection", frame)
+        return img
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+webrtc_streamer(
+    key="drowsiness-detection",
+    video_transformer_factory=VideoTransformer,
+    media_stream_constraints={"video": True, "audio": False}
+)
